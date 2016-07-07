99b9ce245fcb57fb48b5946547190bd61e9514bd
laraga@rivier.edu <laraga@rivier.edu>
2016-07-07 09:27:21 -0400

    Initial Commit

--- app/src/main/java/com/example/lucky/itzme/MainActivity.java
+++ app/src/main/java/com/example/lucky/itzme/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.lucky.itzme;
 
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.capturebtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(takePictureIntent);
+
+            }
+        });
+
     }
 }
