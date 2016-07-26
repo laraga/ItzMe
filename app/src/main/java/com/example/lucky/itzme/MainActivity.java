58e321a68bda1e4fb2297c39f08964024033500f
laraga@rivier.edu <laraga@rivier.edu>
2016-07-25 22:02:58 -0400

    Updated changes to create signature, create zip and verify zip. Still uses hardcoded paths.

--- app/src/main/java/com/example/lucky/itzme/MainActivity.java
+++ app/src/main/java/com/example/lucky/itzme/MainActivity.java
@@ -41,24 +41,75 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        Button extractBtn = (Button)findViewById(R.id.extractbtn);
+        extractBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Info", "Extract Button Clicked");
+
+                String tmpDir=Environment.getExternalStorageDirectory()+"/tmpcamtest";
+                String outDir=Environment.getExternalStorageDirectory()+"/tmpunzipdir";
+
+                try
+                {
+                    ZipUtil.unzip(tmpDir+"/zipFile.zip", outDir);
+                    if(SignatureUtil.verifySignature(outDir, "pubkey", "signedImg", "CAMTEST_IMAGE.jpg"))
+                    {
+                        Toast.makeText(MainActivity.this, "Verified", Toast.LENGTH_LONG).show();
+                        Log.d("Info", "Verified");
+                    }
+                    else
+                    {
+                        Log.d("Info", "Failed");
+                        Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.d("error", e.getMessage(), e);
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        if (imageToUploadUri !=null)
+        if(resultCode == RESULT_OK)
         {
-             //Image captured and saved to fileUri specified in Intent
+            if (imageToUploadUri != null)
+            {
+                //Image captured and saved to fileUri specified in Intent
 
-             Log.d("Itzme", "Image Captured");
-            Toast.makeText(this, "image saved to:\n" + imageToUploadUri, Toast.LENGTH_LONG).show();
-        }
-        if(imageToUploadUri ==null)
-        {
-            //Image captured and saved to fileUri specified in Intent
+                Log.d("Itzme", "Image Captured");
+                Toast.makeText(this, "image saved to:\n" + imageToUploadUri, Toast.LENGTH_LONG).show();
+                String tmpDir = Environment.getExternalStorageDirectory() + "/tmpcamtest";
 
-            Toast.makeText(this, "No image saved to:\n" + imageToUploadUri, Toast.LENGTH_LONG).show();
-            Log.d("Itzme", "Image not Captured");
+                try
+                {
+                    byte[] sigBytes = SignatureUtil.signFile(tmpDir + "/privkey", tmpDir + "/CAMTEST_IMAGE.jpg");
+                    SignatureUtil.writeBytesToFile(sigBytes, tmpDir + "/signedImg");
+                    ZipUtil.zip(tmpDir, "CAMTEST_IMAGE.jpg", "signedImg", "pubkey", "zipFile.zip");
+
+                    Toast.makeText(MainActivity.this, "Zipfile Created", Toast.LENGTH_LONG).show();
+                }
+                catch (Exception e)
+                {
+                    Log.d("error", e.getMessage(), e);
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+            else
+            {
+                //Image captured and saved to fileUri specified in Intent
+
+                Toast.makeText(this, "No image saved to:\n" + imageToUploadUri, Toast.LENGTH_LONG).show();
+                Log.d("Itzme", "Image not Captured");
+            }
         }
 
         if(resultCode == RESULT_CANCELED)
